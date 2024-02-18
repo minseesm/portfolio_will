a68b9bbeaabd13749429bb55d2f500acd3181e8a
MiNew_Dev <devminew@MiNew-Devui-MacBookAir.local>
2024-02-17 20:59:46 -0800

    240217 Main Page

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,29 @@
+const headerBtn = document.querySelector(".mobile-btn");
+const headerMenu = document.querySelector(".fixed-header > .menu");
+
+headerBtn.addEventListener("click", () => {
+  headerMenu.classList.toggle("show");
+});
+document.addEventListener("DOMContentLoaded", () => {
+  if (window.screen.width <= 767) {
+    document.querySelector(".fixed-header").classList.add("mobile");
+  } else {
+    document.querySelector(".fixed-header").classList.remove("mobile");
+  }
+});
+
+const blogUrl =
+  "https://www.googleapis.com/blogger/v3/blogs/2399953/posts/bypath?path=/2011/08/latest-updates-august-1st.html&key=AIzaSyCNHLGddjqvCrzOIBF_jjW11YhDQjkMwRw";
+
 window.onload = function () {
   document.querySelector(".background-img").classList.add("show");
+
+  var returnData = fetchBlog(blogUrl);
+  console.log(returnData);
+};
+
+const fetchBlog = async (url) => {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
 };
